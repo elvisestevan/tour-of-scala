adb52995daa97f48675de56adf848b4c94b0ec59
Elvis Estevan <elvis.estevan65@gmail.com>
2018-03-22 19:33:06 -0300

    Interagindo com case class

--- src/main/scala/intro/InteragindoComClasse.scala
+++ src/main/scala/intro/InteragindoComClasse.scala
@@ -4,6 +4,10 @@ object InteragindoComClasse extends App {
 
   val greeter = new Greeter("Hello, ", "!")
 
-  greeter.greet("Elvis Estevan")
+  greeter.greet("Elvis Estevancase class Point(x: Int, y: Int)")
+
+  val point = Point(1, 2)
+
+  println(point)
 
 }
---
+++ src/main/scala/intro/Point.scala
@@ -0,0 +1,3 @@
+package intro
+
+case class Point(x: Int, y: Int)
